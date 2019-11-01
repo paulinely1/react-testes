75460cd08386cb772adba882d943d0f5adadbdde
JPG08-Morgan <paulinelym@gmail.com>
2019-11-01 17:44:07 -0300

    incio coleta de comments

--- src/pages/tc/index.js
+++ src/pages/tc/index.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
- import './styles.css';
+import './styles.css';
 
 export default class Repo extends Component{
+    
     state = {
+        loading: true,
         news: {},
-        loading: true
+        comments: {},
+        comments_loading: true
     }
 
     async componentDidMount(){
         const Api = require('../../services/api');
         const {user_id} = this.props.match.params;
 
-        const response = await Api.tc.get(`${user_id}?page=0&limit=20`);
+        const response = await Api.tc_ideas.get(`${user_id}?page=0&limit=10`);
         this.setState({ news: response.data, loading: false });
     }
+
+    async getComments(uri){
+        const Api = require('../../services/api');
+        //const {idea_id} = this.props.match.params;
+        //const response = await Api.tc_comments.get(`${idea_id}?limit=1000&since=0`);
+        const response = await Api.tc_comments.get(uri);
+        this.setState({ comments: response.data, comments_loading: false });
+        console.log(this.state.comments);
+    } 
 
     render(){
         const {news, loading} = this.state;
@@ -25,7 +37,9 @@ export default class Repo extends Component{
                {news.map(news_item => (
                    <article key={news_item.id}>
                        <span><strong>{news_item.title}</strong> em: {(new Date(news_item.create_at).getDate())+'/'+(new Date(news_item.create_at).getMonth()+1)}</span>
+                       <p>{news_item.tickers}</p>
                        <div className="post__content" dangerouslySetInnerHTML={{__html: news_item.content}}></div>
+                       <Link onClick={this.getComments(`${news_item.id}?limit=1000&since=0`)}><span className="comments">comentários ({news_item.comments})</span></Link>
                    </article>
                ))}
                <div className="actions">
--- src/services/api.js
+++ src/services/api.js
@@ -4,11 +4,16 @@ const git = axios.create({
     baseURL: 'https://api.github.com'
 });
 
-const token = '3cuih5hgubd8dyjdang9oaq8mh';
+const token = 't4gqy3kyypghijwm4yhpxgny9w';
 
-const tc = axios.create({
+const tc_ideas = axios.create({
     baseURL: 'https://tc.tradersclub.com.br/ideas/api/list/user/',
     headers: {'Authorization': `BEARER ${token}`}
 });
 
-export { git, tc }
+const tc_comments = axios.create({
+    baseURL: 'https://tc.tradersclub.com.br/ideas/api/comment/',
+    headers: {'Authorization': `BEARER ${token}`}
+});
+
+export { git, tc_ideas, tc_comments }
